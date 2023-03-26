4b092e9dc9708b1430dbe14b4ca7cbe6cf077981
Arsh-Pratap <Arshpratap110@gmail.com>
2023-03-26 14:58:30 +0530

    Final

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,51 +1,84 @@
 import React from "react";
 import "./Footer.css";
-import comapanyLogo from "../../assets/logo.svg"
-import facebook from "../../assets/facebook.svg"
-import instagram from "../../assets/instagram.svg"
-import linkedin from "../../assets/linkedin.svg"
-import twitter from "../../assets/twitter.svg"
+import comapanyLogo from "../../assets/logo.svg";
+import facebook from "../../assets/facebook.svg";
+import instagram from "../../assets/instagram.svg";
+import linkedin from "../../assets/linkedin.svg";
+import twitter from "../../assets/twitter.svg";
 
 const Footer = () => {
   return (
     <div className="footer-section container" id="contact">
       <div className="footer-header">
         <div className="footer-logo-column">
-        <img src={comapanyLogo} alt="company-logo" className="logo"/>
-        <p>A new way to make the payments easy, reliable and secure.</p>
+          <img src={comapanyLogo} alt="company-logo" className="logo" />
+          <p>A new way to make the payments easy, reliable and secure.</p>
         </div>
         <div className="footer-column">
           <h3>Usefull Links</h3>
-          <p>Content</p>
-          <p>How it Works</p>
-          <p>Create</p>
-          <p>Explore</p>
-          <p>Terms & Services</p>
+          <a href="#footer">
+            <p>Content</p>
+          </a>
+          <a href="#footer">
+            <p>How it Works</p>
+          </a>
+          <a href="#footer">
+            <p>Create</p>
+          </a>
+          <a href="#footer">
+            <p>Explore</p>
+          </a>
+          <a href="#footer">
+            <p>Terms & Services</p>
+          </a>
         </div>
 
         <div className="footer-column">
           <h3>Community</h3>
-          <p>Help Center</p>
-          <p>Partners</p>
-          <p>Suggestions</p>
-          <p>Blog</p>
-          <p>Newsletters</p>
+          <a href="#footer">
+            <p>Help Center</p>
+          </a>
+          <a href="#footer">
+            <p>Partners</p>
+          </a>
+          <a href="#footer">
+            <p>Suggestions</p>
+          </a>
+          <a href="#footer">
+            <p>Blog</p>
+          </a>
+          <a href="#footer">
+            <p>Newsletters</p>
+          </a>
         </div>
 
         <div className="footer-column">
           <h3>Partner</h3>
-          <p>Our Partner</p>
-          <p>Become a Partner</p>
+          <a href="#footer">
+            <p>Our Partner</p>
+          </a>
+          <a href="#footer">
+            <p>Become a Partner</p>
+          </a>
         </div>
       </div>
       <div className="footer-bottom">
-      <p>Made by Arsh</p>  <div className="social-icons">
-      <a href="#footer" ><img src={instagram} alt="fb" /></a>
-      <a href="#footer" ><img src={facebook} alt="fb" /></a>
-      <a href="#footer" ><img src={twitter} alt="fb" /></a>
-      <a href="#footer" ><img src={linkedin} alt="fb" /></a>
-      </div></div>
-      
+        <p>Made by Arsh</p>
+        <div className="social-icons">
+          <a href="#footer">
+            <img src={instagram} alt="fb" />
+          </a>
+          <a href="#footer">
+            <img src={facebook} alt="fb" />
+          </a>
+          <a href="#footer">
+            <img src={twitter} alt="fb" />
+          </a>
+          <a href="#footer">
+            <img src={linkedin} alt="fb" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
